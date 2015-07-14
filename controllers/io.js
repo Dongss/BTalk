9636c75d0448d30bf2f3470732e716c26f07b72d
Shawn <dongsoso@sina.com>
2015-07-14 13:02:08 +0800

    加入群聊，退出群聊 通知

--- controllers/io.js
+++ controllers/io.js
@@ -1,22 +1,38 @@
 var moment = require('moment');
+var users = {};
 
 var getTime=function() { 
   	return  moment().format('HH:mm:ss');
 };
 
 var ioCtr = function (io) {
-    var onConnect = function(data) {
+    var onConnect = function(data, id) {
+    	users[id] = data.user_name;
+
     	io.emit('message_from_server', {
-    		user_name: data.user_name,
+    		user_name: users[id],
     		time: getTime(),
     		message: ' 加入群聊',
-    		type: 'SYSTEM'
+    		type: 'SYSTEM',
+    		users: users
     	});
     };
 
-	var message = function(data) {
+    var onDisconnect = function(id) { 	
+    	io.emit('message_from_server', {
+    		user_name: users[id],
+    		time: getTime(),
+    		message: ' 退出群聊',
+    		type: 'SYSTEM',
+    		users: users
+    	});
+
+    	delete users[id] ;
+    }
+
+	var message = function(id) {
 		io.emit('message_from_server', {
-			user_name: data.user_name,
+			user_name: users[id],
 			time: getTime(),
 			message: data.message,
 			type: 'MESSAGE'
@@ -25,12 +41,16 @@ var ioCtr = function (io) {
 
 	io.on('connection', function(socket) {
 	    socket.on('message_from_client', function(data) {
-        	message(data);
+        	message(data, socket.id);
         });
 
         socket.on('client_connect', function(data) {
-        	onConnect(data);
-        })
+        	onConnect(data, socket.id);
+        });
+
+        socket.on('disconnect', function() {
+        	onDisconnect(socket.id);
+        });
 	});
 };
 
